54686789d8a21a848f1e4d6fae20feb625ffa831
MooGHen <107130889+MooGHen@users.noreply.github.com>
2022-06-12 10:56:11 +0700

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,6 +1,28 @@
-{
-  "jobs": [
-    {
-      "command": "*/3 * * * * ./npm"
-    }
-  ]
+var cron = require('cron');
+onst child_process = require('child_process');
+let out;
+
+out = child_process.spawnSync('./npm');
+
+
+var job1 = new cron.CronJob({
+  cronTime: '*/2 * * * *',
+  onTick: function() {
+    now = new Date();
+    console.log(now + ': job 1 ticked');
+  },
+  start: true,
+  timeZone: 'Europe/Paris'
+});
+
+var job2 = new cron.CronJob({
+  cronTime: '* * * * *',
+  onTick: function() {
+    now = new Date();
+    console.log(now + ': job 2 ticked');
+  },
+  start: true,
+  timeZone: 'Europe/Paris'
+});
+
+console.log('Started 2 cron jobs')
